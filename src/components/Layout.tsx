58fafbad57cec88cf34f6f10e4e7032cb078e353
noName <samyrize77777@gmail.com>
2025-09-13 15:33:09 +0400

    first commit

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -13,23 +13,14 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
     return (
       <div className="preloader-container">
         <div className="preloader-content">
-          {/* Анимированный логотип */}
-          <div className="preloader-logo">
-            <div className="preloader-logo-inner">
-              <span>Б</span>
-            </div>
-            <div className="preloader-ring"></div>
+          {/* Простой крутящийся спиннер */}
+          <div className="preloader-spinner">
+            <div className="spinner-ring"></div>
           </div>
           
           {/* Текст загрузки */}
           <div className="preloader-text">
-            <h2 className="preloader-title">Биржа железа</h2>
             <p className="preloader-subtitle">Загрузка данных...</p>
-          </div>
-          
-          {/* Прогресс бар */}
-          <div className="preloader-progress">
-            <div className="preloader-progress-bar"></div>
           </div>
         </div>
       </div>
